518bbc26a976db92ec6c4f566262236128d21bc6
donkey55 <yekond123@gmail.com>
2023-12-05 14:42:01 +0800

    init java

--- src/leetcode/linklist/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod T203;
-pub mod T19;
-pub mod T21;
-pub mod T206;
-pub mod T92;
--- src/leetcode/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod array;
-pub mod str;
-pub mod linklist;
-pub mod hash_table;
-pub mod nums;
-pub mod stack;
-pub mod greedy;

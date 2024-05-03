fccad25d1b226c643fea366a4b30e02a4c4786b0
lsp <lvshuopeng@icloud.com>
2024-05-03 18:29:16 +0800

    fix: change directory

--- main.go
+++ main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"context"
 	"sync"
 )
 
 func main() {
 	var rw sync.RWMutex
 	rw.Lock()
-	ctx, cancelFunc := context.WithCancel(context.TODO())
-	cancelFunc()
+	//ctx, cancelFunc := context.WithCancel(context.TODO())
+	//cancelFunc()
 
 }
